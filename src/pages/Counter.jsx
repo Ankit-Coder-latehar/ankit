edcf85df90b7dda194526d836fd15a1a04b850c6
Ankit-Coder-latehar <148073889+Ankit-Coder-latehar@users.noreply.github.com>
2024-08-21 15:57:55 +0530

    Update Counter.jsx

--- src/pages/Counter.jsx
+++ src/pages/Counter.jsx
@@ -6,8 +6,8 @@ import ContractCard from '../components/Dashboard/ContractedLane';
 import TableHeader from '../components/Dashboard/TableHeader';
 
 const Counter = () => {
-  // Determine the number of ContractCard components to display
-  const contractCardCount = 2; // You can adjust this count as needed
+
+  const contractCardCount = 2; 
 
   return (
     <div className='absolute top-0 left-0 w-full h-full m-0 p-0 bg-white'>
